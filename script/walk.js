79d9edcf65c03dbeb64e7e6a840c8a2cda161bc5
mehrzad-nava <mehrzadnobitexx@gmail.com>
2024-09-04 02:15:55 +0430

    Change the walking buttons

--- script/walk.js
+++ script/walk.js
@@ -5,14 +5,14 @@ let left = 0;
 document.onkeydown = (e) => {
     e = e || window.event;
 
-    if (e.keyCode == "39") {
+    if (e.keyCode == "68") {
         left += 10;
         ninja.style.left = left + "px";
 
         ninja.classList.remove("ninja_stand_right", "ninja_stand_left");
         ninja.classList.add("ninja_walk_right");
         ninja.style.transform = "scaleX(1)";
-    } else if (e.keyCode == "37") {
+    } else if (e.keyCode == "65") {
         left -= 10;
         ninja.style.left = left + "px";
 
@@ -23,7 +23,7 @@ document.onkeydown = (e) => {
 };
 
 document.onkeyup = (e) => {
-    if (e.keyCode == "39" || e.keyCode == "37") {
+    if (e.keyCode == "68" || e.keyCode == "65") {
         ninja.classList.remove("ninja_walk_right");
         ninja.classList.add("ninja_stand_right");
     }
