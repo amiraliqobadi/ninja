17db5d6f28e108a8e6c377ddcb7674fd8057a02b
amiraliqobadi <amiraliqobadi5@gmail.com>
2024-09-03 19:26:51 +0330

    v 1.3

--- script/walk.js
+++ script/walk.js
@@ -1,7 +1,6 @@
 let ninja = document.getElementById("ninja");
 
 let left = 0;
-let walkTimeout;
 
 document.onkeydown = (e) => {
     e = e || window.event;
@@ -10,17 +9,22 @@ document.onkeydown = (e) => {
         left += 10;
         ninja.style.left = left + "px";
 
-        ninja.classList.remove("ninja_stand");
+        ninja.classList.remove("ninja_stand_right", "ninja_stand_left");
         ninja.classList.add("ninja_walk_right");
+        ninja.style.transform = "scaleX(1)";
     } else if (e.keyCode == "37") {
         left -= 10;
         ninja.style.left = left + "px";
+
+        ninja.classList.remove("ninja_stand_right", "ninja_stand_left");
+        ninja.classList.add("ninja_walk_right");
+        ninja.style.transform = "scaleX(-1)";
     }
 };
 
 document.onkeyup = (e) => {
-    if (e.keyCode == "39") {
+    if (e.keyCode == "39" || e.keyCode == "37") {
         ninja.classList.remove("ninja_walk_right");
-        ninja.classList.add("ninja_stand");
+        ninja.classList.add("ninja_stand_right");
     }
 };
