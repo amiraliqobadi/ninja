eb48fd8ea457e037169085436c12dc0b4672f094
amiraliqobadi <amiraliqobadi5@gmail.com>
2024-09-04 04:10:13 +0330

    gravity and jump

--- script/walk.js
+++ script/walk.js
@@ -1,30 +1,70 @@
 let ninja = document.getElementById("ninja");
-
 let left = 0;
+let bottom = 26;
+let isJumping = false;
+let gravity = 2;
+let gravityInterval;
+let keys = {};
+
+function startGravity() {
+    if (!gravityInterval) {
+        gravityInterval = setInterval(applyGravity, 60);
+    }
+}
+
+function applyGravity() {
+    if (bottom > 26) {
+        bottom -= gravity;
+        if (bottom <= 26) {
+            bottom = 26;
+            clearInterval(gravityInterval);
+            gravityInterval = null;
+            isJumping = false;
+            ninja.classList.add("ninja_stand_right");
+        }
+        ninja.style.bottom = bottom + "%";
+    }
+}
 
 document.onkeydown = (e) => {
     e = e || window.event;
+    keys[e.keyCode] = true;
 
-    if (e.keyCode == "68") {
-        left += 15;
+    if (keys[68]) {
+        isJumping === true ? (left += 50) : (left += 15);
         ninja.style.left = left + "px";
-
-        ninja.classList.remove("ninja_stand_right", "ninja_stand_left");
+        ninja.classList.remove("ninja_stand_right");
         ninja.classList.add("ninja_walk_right");
         ninja.style.transform = "scaleX(1)";
-    } else if (e.keyCode == "65") {
-        left -= 15;
-        ninja.style.left = left + "px";
+    }
+    if (keys[65]) {
+        isJumping === true ? (left -= 50) : (left -= 15);
 
-        ninja.classList.remove("ninja_stand_right", "ninja_stand_left");
+        ninja.style.left = left + "px";
+        ninja.classList.remove("ninja_stand_right");
         ninja.classList.add("ninja_walk_right");
         ninja.style.transform = "scaleX(-1)";
+    }
+    if (keys[87] && !isJumping) {
+        isJumping = true;
+        bottom += 50;
+        ninja.style.bottom = bottom + "%";
+        clearInterval(gravityInterval);
+        gravityInterval = null;
     }
 };
 
 document.onkeyup = (e) => {
-    if (e.keyCode == "68" || e.keyCode == "65") {
+    e = e || window.event;
+    keys[e.keyCode] = false;
+    if (!keys[65] && !keys[68]) {
         ninja.classList.remove("ninja_walk_right");
-        ninja.classList.add("ninja_stand_right");
+        if (bottom === 26 && !isJumping) {
+            ninja.classList.add("ninja_stand_right");
+        }
+    }
+
+    if (!keys[87]) {
+        startGravity();
     }
 };
